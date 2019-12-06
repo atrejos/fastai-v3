17b5474241559ea1719e3dcf311332c95d004116
AlejandroTrejos <51036952+AlejandroTrejos@users.noreply.github.com>
2019-12-06 00:31:17 -0600

    updated for ASL classifier

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1dzscp0CsjH0pTXO8tAPtMVet5Dg0PXnR?alt=media&key=AIzaSyC4BxAWsOLTIOzSvjobYZNeAPxBNbk5yGQ'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y']
 path = Path(__file__).parent
 
 app = Starlette()
